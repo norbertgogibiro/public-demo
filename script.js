40f7e1790574dd6479efd6b8ee0fb477b25c8916
Norbert Biro <norbert@wannasport.dk>
2020-09-19 14:03:18 +0200

    Adding current year to their containers

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openDropdownClass = 'dropdown-state-open';
 const dropdownOpenerClass = 'dropdown-opener';
 const emailLinkClass = 'link-email';
+const currentYearContainerClass = 'current-year';
 const clickEvents = ['click', 'touchstart'];
 
 const setEyeBallPosition = function ({ clientX, clientY }) {
@@ -99,4 +100,9 @@ document.querySelectorAll(`.${emailLinkClass}`).forEach(emailLink => {
         window.location.href = `mailto:${name}@${domain}.${tld}`;
         return false;
     }));
+});
+
+// Populate current year:
+document.querySelectorAll(`.${currentYearContainerClass}`).forEach(currentYearContainer => {
+    currentYearContainer.innerHTML = new Date().getFullYear();
 });
