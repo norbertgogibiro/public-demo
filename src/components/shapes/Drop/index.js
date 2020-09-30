2d6c15f51763af370e1bccd3a227145a7044fb06
Norbert Biro <norbert@wannasport.dk>
2020-09-30 22:19:54 +0200

    Setting up new SASS structure

---
+++ src/components/shapes/Drop/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Shape from './shape.svg'
+import './styles.scss';
+
+const Drop = () => {
+  return (
+    <Shape className='drop-shape'/>
+  )
+}
+
+export default Drop;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Drop from './components/shapes/Drop';
 import './utils/faviconLoader';
+import './index.scss';
 
 const App = () => {
   return (
     <div>
       <div>Welcome to my-webpack-react-starter</div>
+      <Drop/>
     </div>
   );
 };
