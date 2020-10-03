17081c768be12d6a0d9732c787a4123786f250a7
Norbert Biro <norbert@wannasport.dk>
2020-10-03 19:11:00 +0200

    Cross-browser-friendly SVG path animation

--- src/components/misc/index.js
+++ src/components/misc/index.js
@@ -1,2 +1,4 @@
 export { default as AppContext } from './_AppContext';
 export { default as ErrorBoundary } from './_ErrorBoundary';
+export { default as SvgPathAnimation } from './_SvgPathAnimation';
+export { default as SvgWrapper } from './_SvgWrapper';
