10a8794dd840ea23531ee4b48ac337c6dcc346b9
Norbert Biro <norbert@wannasport.dk>
2020-10-03 16:37:18 +0200

    Toggling the dropdown (React way)

--- src/components/elements/Dropdown/index.js
+++ src/components/elements/Dropdown/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
+import { clickEvents } from '../../../common/globalVariables';
+
+const closerEvents = [...clickEvents, 'keyup'];
 
 const Dropdown = ({ openerButtonText, children }) => {
+	const [dropdownOpenState, setDropdownOpenState] = useState(false);
+	const dropdownWrapperClasses = ['dropdown-container'];
+	const handleDropdownClosing = ({ key, type }) => {
+		if (clickEvents.includes(type) || (type === 'keyup' && key === 'Escape')) {
+			setDropdownOpenState(false);
+			closerEvents.forEach(eventName => document.removeEventListener(eventName, handleDropdownClosing));
+		}
+	}
+
+	if (dropdownOpenState) {
+		dropdownWrapperClasses.push('dropdown-state-open');
+	}
+
+	useEffect(() => {
+		if (dropdownOpenState) {
+			closerEvents.forEach(eventName => document.addEventListener(eventName, handleDropdownClosing));
+		}
+
+		return function cleanUp() {
+			closerEvents.forEach(eventName => document.removeEventListener(eventName, handleDropdownClosing));
+		}
+	}, [dropdownOpenState]);
+
 	return (
-		<div className="dropdown-container">
-			<button className="dropdown-opener">
+		<div className={dropdownWrapperClasses.join(' ')}>
+			<button
+				className="dropdown-opener"
+				onClick={() => setDropdownOpenState(!dropdownOpenState)}
+			>
 				{openerButtonText}
 			</button>
 
@@ -17,7 +46,8 @@ const Dropdown = ({ openerButtonText, children }) => {
 };
 
 Dropdown.propTypes = {
-	openerButtonText: PropTypes.string.isRequired
+	openerButtonText: PropTypes.string.isRequired,
+	children: PropTypes.element
 };
 
 export default Dropdown;
