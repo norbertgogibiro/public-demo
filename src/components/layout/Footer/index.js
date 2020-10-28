1aad5bc53cd57eb302d4d5e16f55f91661e11323
Norbert Biro <norbert@wannasport.dk>
2020-10-28 15:00:28 +0100

    Changing the author's name in the footer

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -5,7 +5,7 @@ import { LinkUrl } from '../../elements';
 const Footer = () => (
 	<footer>
 		<small>
-			<p>A website made by <LinkUrl label='norbertgogibiro' href='https://dk.linkedin.com/in/norbert-biro'/> based on <LinkUrl label='Saba Anwar' href='https://fr.linkedin.com/in/sabanoir'/>&apos;s Gazsiafter animation theme design</p>
+			<p>A website made by <LinkUrl label='this guy' href='https://dk.linkedin.com/in/norbert-biro'/> based on <LinkUrl label='Saba Anwar' href='https://fr.linkedin.com/in/sabanoir'/>&apos;s Gazsiafter animation theme design</p>
 			<p>Copyright &copy; <span className="current-year"></span> Paperdeer. All Rights Reserved</p>
 		</small>
 	</footer>
