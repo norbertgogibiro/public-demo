16ba4514af95883fa3244cd2a92af42d075991b3
Norbert Biro <norbert@wannasport.dk>
2020-11-04 21:46:21 +0100

    Fixing missing email links

--- src/components/elements/LinkEmail/index.js
+++ src/components/elements/LinkEmail/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './styles.scss';
 
-const LinkEmail = ({ user, domain, tld }) => (
-	<a
-		href="#"
-		className='link-email'
-		data-user={user}
-		data-domain={domain}
-		data-tld={tld}
-	/>
-);
+const LinkEmail = ({ user, domain, tld }) => {
+	const [finalLink, setFinalLink] = useState('');
+
+	useEffect(() => {
+		setFinalLink(`mailto:${user}@${domain}.${tld}`);
+	}, []);
+
+	return (
+		<a
+			href={finalLink}
+			className='link-email'
+			data-user={user}
+			data-domain={domain}
+			data-tld={tld}
+		/>
+	);
+}
 
 LinkEmail.propTypes = {
 	user: PropTypes.string.isRequired,
