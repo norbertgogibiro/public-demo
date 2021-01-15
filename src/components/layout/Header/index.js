01a6985e99ea84ca6495aed48fead1a9aebfc76e
Norbert Biro <norbert@wannasport.dk>
2021-01-15 12:21:26 +0100

    Embracing users to visit Paperdeer catalogs on better paying services

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -4,13 +4,13 @@ import { LinkUrl, LinkEmail, Dropdown, SiteLogo } from '../../elements';
 
 const navMenuItems = {
 	Music: [
-		{ label: 'Spotify', href: 'https://open.spotify.com/artist/2dvKZH9pulH1g2m26HkoIx' },
-		{ label: 'Soundcloud', href: 'https://soundcloud.com/paperdeer' },
+		{ label: 'Tidal', href: 'https://tidal.com/browse/artist/5352971', isEnhanced: true },
+    { label: 'Apple Music', href: 'https://itunes.apple.com/us/artist/paperdeer/795131316', isEnhanced: true},
 		{ label: 'Deezer', href: 'https://www.deezer.com/en/artist/5468085' },
-		{ label: 'Tidal', href: 'https://tidal.com/browse/artist/5352971' },
-		{ label: 'Apple Music', href: 'https://itunes.apple.com/us/artist/paperdeer/795131316' },
-		{ label: 'Google Play', href: 'https://play.google.com/store/music/artist/Paperdeer?id:Amflr2k6cb5h5z4rqsda75juzt4' },
-		{ label: 'Amazon Music', href: 'https://www.amazon.com/s?k:Paperdeer&i:digital-music&dc&ref:a9_asc_1' }
+		{ label: 'Amazon Music', href: 'https://www.amazon.com/s?k:Paperdeer&i:digital-music&dc&ref:a9_asc_1' },
+		{ label: 'Spotify', href: 'https://open.spotify.com/artist/2dvKZH9pulH1g2m26HkoIx' },
+		{ label: 'Youtube Music', href: 'https://music.youtube.com/channel/UCI-cVxCzSggq8Yk-9s-oEKQ' },
+		{ label: 'Soundcloud', href: 'https://soundcloud.com/paperdeer' }
 	],
 	News: [
 		{ label: 'Facebook', href: 'https://www.facebook.com/paperdeermusic' },
